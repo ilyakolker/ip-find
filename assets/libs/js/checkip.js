d5362e3af491420fffce4783fc5950c214e79278
ilya1989 <ilyakolker@gmail.com>
2018-12-09 23:54:56 +0200

    cool working web

--- assets/libs/js/checkip.js
+++ assets/libs/js/checkip.js
@@ -1,50 +1,58 @@
 $("#loader").removeClass("outer");
+
+const dataArry = [];
 $('#run').on('click',function(){
   $("#loader").addClass("outer");
-let xhr = new XMLHttpRequest();
-let ipinput = ip_input.value;
-const dataArry = []; 
-dataArry.length = 4;
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "http://proxycheck.io/v2/"+ipinput+"?key=446704-298402-570002-06g06y&vpn=1&asn=1&node=1&time=1&inf=0&port=1&seen=1&days=7&tag=msg";
-xhr.open("GET",proxyurl+url); // assuming you’re hosting it locally
-xhr.send()
-xhr.onreadystatechange=(e)=>{
-let ip =  JSON.parse(xhr.responseText);
-if (ip[ipinput].proxy == "yes") {
-dataArry[0] = "Country: " + ip[ipinput].country;
-dataArry[1] = "provider: " +ip[ipinput].provider;
-dataArry[2] = "asn: " +ip[ipinput].asn;
-dataArry[3] = "isocode: " +ip[ipinput].isocode;
-var ul = document.createElement("ul");
+  $.get("https://cors-anywhere.herokuapp.com/https://proxycheck.io/v2/"+ip_input.value+"?vpn=1&asn=1&tag=proxycheck.io", function(data) {
+    console.log(data);
+    if (data.status === "error") {
+      Loaderoff();
+      $("#title").text(data.message);
+      $("#title").addClass("bg-info");
+      
+    }else{
+      if (data[ip_input.value].proxy == "yes") {
+        Loaderoff();
+        BuildData(data);
+        $("#title").text("Proxy Detected");
+        $("#title").addClass("bg-danger");
+        
+        }else {
+        Loaderoff();
+        BuildData(data);
+        $("#title").text("Proxy Not detected");
+        $("#title").addClass("bg-success");
+        }
+    }
 
-for (let i = 0; i < dataArry.length; i++) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(dataArry[i]));
-    ul.appendChild(li);
-}
-$("#ip-data").html(ul);
-$("#title").removeClass("bg-success");
-$("#loader").removeClass("outer");
-$("#title").text("Proxy Detected");
-$("#title").addClass("bg-danger");
-}else {
-dataArry[0] = "Country: " + ip[ipinput].country;
-dataArry[1] = "provider: " +ip[ipinput].provider;
-dataArry[2] = "asn: " +ip[ipinput].asn;
-dataArry[3] = "isocode: " +ip[ipinput].isocode;
-var ul = document.createElement("ul");
+  }, 'json');
+ });
 
-for (let i = 0; i < dataArry.length; i++) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(dataArry[i]));
-    ul.appendChild(li);
-}
-$("#ip-data").html(ul);
-$("#title").removeClass("bg-danger");
-$("#loader").removeClass("outer");
-$("#title").text("Proxy Not detected");
-$("#title").addClass("bg-success");
-}
-}
-});
+ function Loaderoff(){
+   $("#ip-data").html("");
+  $("#title").removeClass("bg-info");
+  $("#title").removeClass("bg-success");
+  $("#title").removeClass("bg-danger");
+  $("#loader").removeClass("outer");
+ }
+
+ function BuildData(data){
+  dataArry[0] = "Country: " + data[ip_input.value].country;
+  dataArry[1] = "provider: " +data[ip_input.value].provider;
+  dataArry[2] = "asn: " +data[ip_input.value].asn;
+  dataArry[3] = "isocode: " +data[ip_input.value].isocode;
+  var ul = document.createElement("ul");
+  
+  for (let i = 0; i < dataArry.length; i++) {
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(dataArry[i]));
+      if(i === 3){
+        var flag = document.createElement("i");
+        flag.setAttribute("class","flag flag-"+ data[ip_input.value].isocode.toLowerCase())
+        li.appendChild(flag);
+      }
+      
+      ul.appendChild(li);
+  }
+  $("#ip-data").html(ul);
+ }
